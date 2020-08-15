74e80a4ee333fab7fa4e5cb06d986cc220c905cf
anmoldav <anmoldavane@gmail.com>
2020-08-15 16:43:51 +0530

    Inital commit

---
+++ src/test/java/com/anmol/smsemailservice/SmsemailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol.smsemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsemailserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
