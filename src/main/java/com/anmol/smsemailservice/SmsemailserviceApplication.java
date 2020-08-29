e421f7dd718af4093c20fd88c349d5528fa6aafe
anmoldav <anmoldavane@gmail.com>
2020-08-30 00:22:14 +0530

    Enabling Swagger

--- src/main/java/com/anmol/smsemailservice/SmsemailserviceApplication.java
+++ src/main/java/com/anmol/smsemailservice/SmsemailserviceApplication.java
@@ -1,16 +1,27 @@
 package com.anmol.smsemailservice;
 
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SmsemailserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsemailserviceApplication.class, args);
 
-	 
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.anmol.smsemailservice")).build();
 	}
 
 }
